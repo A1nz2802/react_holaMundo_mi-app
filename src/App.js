3a3cb26c197cb16a6585c627c38f678b511c31f1
Brawer Nuñez <n.eedar@hotmail.com>
2021-10-03 00:05:32 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+/* 
+import Button from "./Button";
 
-function App() {
+const arr = [
+  'chanchito feliz',
+  'chanchito triste',
+  'chanchito emocionado',
+]
+const App = () => {
+  const miVariable = false;
+  // Reenderizado condicional
+  if (miVariable) {
+    return <p>Mi variable dio true!</p>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 onClick={(e) => console.log('click', e)}>Hola Mundo</h1>
+      { arr.map(el => <p key={el}>{el}</p> )}
+      <Button onClick={() => console.log('clickeado')}>
+        Enviar
+      </Button>
     </div>
-  );
+  )
+}
+*/
+import { Component } from "react";
+
+class Button extends Component {
+  state = {}
+  constructor(props) {
+    super(props)
+    console.log('constructor', props);
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate', prevProps, prevState);
+  }
+
+  componentWillUnmount() {
+    console.log('desmontando componente', this.props, this.state)
+  }
+
+  render() {
+    console.log('run button render')
+    return (
+      <button onClick={() => this.setState({ prop: 1 })}>
+        Enviar
+      </button>
+    )
+  }
+}
+
+
+class App extends Component {
+  // es obligatorio llamarlo "state"
+  state = {
+    valor: 3
+  }
+  /* render() se ejecuta bajo 2 condiciones: cuando hubo un cambio en el estado del componente
+  el otro motivo es cuando el método render de un componente padre ha sido llamado, por ende, el metodo 
+  render de los componentes hijos también serán llamados
+ */
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <p>Hola mundoooooo!!!!!!</p>
+        { this.state.valor === 3
+          ? <Button chanchito='feliz' />
+          : null }
+        <button 
+          className={`${this.state.valor}`}
+          onClick={() => this.setState({ valor: 2 })}
+        >
+          Enviar en App
+        </button>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+/* import reportWebVitals from './reportWebVitals'; */
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +11,42 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+// <p>hola mundo</p>
+
+/* 
+const Li = ({ children, estado, casa, edad }) => {
+  console.log(casa, edad)
+  return (
+    <li>{children} estoy {estado}</li>
+  )
+}
+
+const X = () => 
+  <ul>
+    <Li 
+      estado={'feliz'}
+      casa={false}
+      edad={22}
+    >
+      Chanchito
+    </Li>
+
+    <Li estado={'triste'}>Chanchito Triste</Li>
+    <Li estado={'emocionado'}>Felipe</Li>
+  </ul>
+  
+
+ReactDOM.render(
+  // render recibe: Lo que queremos reenderizar, DONDE lo queremos reenderizar
+  <X />, document.getElementById('root')
+)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
+
+
+JSX no es más que azucar sintáctica para que no esteemos escribiendo constantemente
+React.createElement()
+ */
